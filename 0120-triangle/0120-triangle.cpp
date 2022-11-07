a75ed3cc4bc4ba3b52a4aa6a60c034eb46cee6cc
Lovakush <20051303@kiit.ac.in>
2022-11-07 14:48:42 +0530

    Time: 17 ms (27.52%), Space: 9.1 MB (31.86%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -9,8 +9,25 @@ public:
         return dp[i][j] = min(d,dg);
 }
     int minimumTotal(vector<vector<int>>& triangle) {
+        // int n = triangle.size();
+        // vector<vector<int>> dp(n,vector<int>(n,-1));
+        // return f(triangle,0,0,n,dp);
         int n = triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return f(triangle,0,0,n,dp);
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        int i = n - 1;
+        for (int j = i; j >= 0; j--)
+        {
+            dp[i][j] = triangle[i][j];
+        }
+        for (int i = n - 2; i >= 0; i--)
+        {
+            for (int j = i; j >= 0; j--)
+            {
+                int d = triangle[i][j] + dp[i + 1][j];
+                int dg = triangle[i][j] + dp[i + 1][j + 1];
+                dp[i][j] = min(d, dg);
+            }
+        }
+        return dp[0][0];
     }
 };
